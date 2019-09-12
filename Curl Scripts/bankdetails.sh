04bd503074b80c41c3ecd4c12124ece56009328d
shouryadey <36259414+shouryadey@users.noreply.github.com>
2019-09-13 02:55:48 +0530

    Update bankdetails.sh

--- Curl Scripts/bankdetails.sh
+++ Curl Scripts/bankdetails.sh
@@ -1,6 +1,5 @@
 #https://protected-plateau-55954.herokuapp.com/bankdetails?IFSC_code=ABHY0065001
-#Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNTYwOTE4OTE2LCJleHAiOjE1NjEzNTA5MTZ9.JRcmxx4_47jHXrqy4_249C6UhUVUMEoKYMo6UuZ26f0
-
+#Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNTY4MzIzMTM2LCJleHAiOjE1Njg3NTUxMzZ9.3M0xRclV2QscVrwOcDWe_M3PUjnWUrStyTxsTuD82YU
 char=1
 while [ $char -eq 1 ]
 do
@@ -14,8 +13,7 @@ do
 	offset="offset=$c"
 	URL=$HTTP$IFSC$limit$offset
 	#echo $URL
-	#curl --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNTYwOTE4OTE2LCJleHAiOjE1NjEzNTA5MTZ9.JRcmxx4_47jHXrqy4_249C6UhUVUMEoKYMo6UuZ26f0" http://localhost:3000/bankdetails?IFSC_code=ABHY0065001
-	curl --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNTYwOTE4OTE2LCJleHAiOjE1NjEzNTA5MTZ9.JRcmxx4_47jHXrqy4_249C6UhUVUMEoKYMo6UuZ26f0"$URL
+	curl --header "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXJuYW1lIiwiaWF0IjoxNTY4MzIzMTM2LCJleHAiOjE1Njg3NTUxMzZ9.3M0xRclV2QscVrwOcDWe_M3PUjnWUrStyTxsTuD82YU"$URL
 	echo " "
 	read -p "'0' to quit else '1' to continue : " char
 done
